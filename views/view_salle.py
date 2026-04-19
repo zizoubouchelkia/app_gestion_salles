31feb1f187956d4617c7e2e011fc8e21b0902597
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-04-19 14:26:40 -0400

    Ajout du cadre Informations Salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -10,3 +10,17 @@ class ViewSalle(ctk.CTk):
         self.geometry("700x500")
 
         self.service_salle = ServiceSalle()
+        self.cadreInfo = ctk.CTkFrame(self)
+        self.cadreInfo.pack(pady=10)
+
+        self.entry_code = ctk.CTkEntry(self.cadreInfo, placeholder_text="Code")
+        self.entry_code.pack(pady=5)
+
+        self.entry_libelle = ctk.CTkEntry(self.cadreInfo, placeholder_text="Libelle")
+        self.entry_libelle.pack(pady=5)
+
+        self.entry_type = ctk.CTkEntry(self.cadreInfo, placeholder_text="Type")
+        self.entry_type.pack(pady=5)
+
+        self.entry_capacite = ctk.CTkEntry(self.cadreInfo, placeholder_text="Capacite")
+        self.entry_capacite.pack(pady=5)
