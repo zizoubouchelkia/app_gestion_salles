10e4a80b477f22efdd1b2f6c25d3fdf765398592
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-04-19 14:32:34 -0400

    Ajout des boutons CRUD dans GUI

--- views/view_salle.py
+++ views/view_salle.py
@@ -24,3 +24,23 @@ class ViewSalle(ctk.CTk):
 
         self.entry_capacite = ctk.CTkEntry(self.cadreInfo, placeholder_text="Capacite")
         self.entry_capacite.pack(pady=5)
+
+        self.cadreActions = ctk.CTkFrame(self)
+        self.cadreActions.pack(pady=10)
+
+        ctk.CTkButton(self.cadreActions, text="Ajouter", command=self.ajouter_salle).pack(side="left", padx=5)
+        ctk.CTkButton(self.cadreActions, text="Modifier", command=self.modifier_salle).pack(side="left", padx=5)
+        ctk.CTkButton(self.cadreActions, text="Supprimer", command=self.supprimer_salle).pack(side="left", padx=5)
+        ctk.CTkButton(self.cadreActions, text="Rechercher", command=self.rechercher_salle).pack(side="left", padx=5)
+
+    def ajouter_salle(self):
+        pass
+
+    def modifier_salle(self):
+        pass
+
+    def supprimer_salle(self):
+        pass
+
+    def rechercher_salle(self):
+        pass
