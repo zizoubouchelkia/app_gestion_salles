7ae7e60e4c4fca63432e3429245848b686cbe8a4
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-04-19 14:25:25 -0400

    Creation de la fenetre principale ViewSalle

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,1 +1,12 @@
-# interface graphique
+import customtkinter as ctk
+from services.services_salle import ServiceSalle
+
+
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Gestion des salles")
+        self.geometry("700x500")
+
+        self.service_salle = ServiceSalle()
