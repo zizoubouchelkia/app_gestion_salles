5b8e67355c700d1a9fb23a5f36d3e7c591bebce8
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-04-19 13:55:53 -0400

    Creation de la classe Salle

--- models/salle.py
+++ models/salle.py
@@ -1,1 +1,6 @@
-# classe Salle (a implementer)
+class Salle:
+    def __init__(self, code, libelle, type_salle, capacite):
+        self.code = code
+        self.libelle = libelle
+        self.type = type_salle
+        self.capacite = capacite
