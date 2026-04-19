717188835c28b0fe2d68ff94c379507c36997da3
zizoubouchelkia <zizoubouchelkia9@gmail.com>
2026-04-19 14:07:09 -0400

    Ajout de la validation dans ajouter_salle

--- services/services_salle.py
+++ services/services_salle.py
@@ -6,4 +6,11 @@ class ServiceSalle:
         self.dao_salle = DataSalle()
 
     def ajouter_salle(self, salle):
-        pass
+        if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+            return False, "Tous les champs sont obligatoires"
+
+        if int(salle.capacite) < 1:
+            return False, "Capacité invalide"
+
+        self.dao_salle.insert_salle(salle)
+        return True, "Salle ajoutée avec succès"
